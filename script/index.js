970e77f1727b7b01b0d359b58670b089e4a4f975
Sumiaya Akther <sumiayaakther15@gmail.com>
2025-03-01 09:26:00 +0600

    task

--- script/index.js
+++ script/index.js
@@ -1,7 +1,11 @@
+// page change
+
 document.getElementById("another_page")
 .addEventListener("click", function(){
     window.location.href="./main.html"
 });
+
+// random bg color
 
 document.getElementById("change-bg")
 .addEventListener("click", function(){
@@ -9,3 +13,33 @@ document.getElementById("change-bg")
     .toString(16);
     document.body.style.backgroundColor = randomBg;
 });
+
+
+// dynamic date
+
+document.getElementById("date").innerText = new Date().toDateString();
+
+
+// card section
+
+let navTask = 22;
+const btnCompleted = document.querySelectorAll(".btn-completed");
+let task = btnCompleted.length;
+document.getElementById("taskA").innerText = task;
+
+ for (let i = 0; i < btnCompleted.length; i++) {
+    const btn = btnCompleted[i];
+    btn.addEventListener("click", function(){
+        if (task>0){
+            task--;
+            navTask++;
+
+            document.getElementById("taskA").innerText = task;
+            document.getElementById("nav-task").innerText = navTask;
+        }
+    })
+    
+ }
+   
+
+
